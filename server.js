3eedcc3d20672346edf1132a2738c2baa67feae0
Ghughe <ptp112004@gmail.com>
2023-02-09 00:27:27 +0700

    MVC REST API

--- server.js
+++ server.js
@@ -4,63 +4,28 @@ const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOption");
 
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-const whitelist = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by cors"));
-    }
-  },
-  optionSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
 
-app.use('/subdir', require('./routes/subdir'))
-
-app.use('/', require('./routes/root'))
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-app.get("/old-age(.html)?", (req, res) => {
-  res.redirect("/new-page.html");
-});
-
-// Route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attemp to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hi mum");
-  }
-);
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
     res.sendFile(path.join(__dirname, "views", "404.html"));
   }
   if (req.accepts("json")) {
-    res.json({ error: "404 not found"});
+    res.json({ error: "404 not found" });
   }
 });
 app.use(errorHandler);
